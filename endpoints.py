fbe143e1bb3073eae4f40b0109818aa0853ba694
Marcin <marcin.a.szecowka@gmail.com>
2020-10-07 20:38:46 +0200

    implemented two endpoints

--- endpoints.py
+++ endpoints.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI, Depends
-from uuid import UUID, uuid4
-from utils import generate_random_questions
-from entities import Answer, Question, Survey
-from model import SurveyAnswers, QuestionResponse, SurveyResponse
-from database import engine, Base, SessionLocal
-from sqlalchemy.orm import Session
 from typing import List
+from uuid import UUID, uuid4
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+import utils
+from database import engine, Base, SessionLocal
+from entities import Survey
+from model import SurveyAnswersRequest, SurveyResponse
+from utils import generate_random_questions, is_answer_correct
 
 Base.metadata.create_all(bind=engine)
 
@@ -23,7 +26,7 @@ def get_db():
 @app.post("/surveys", status_code=201, response_model=SurveyResponse)
 async def create_survey(instance_name: str, question_count: int = 2, db: Session = Depends(get_db)):
     survey = Survey()
-    survey.id = uuid4().hex
+    survey.id = str(uuid4())
     survey.instance_name = instance_name
     survey.questions = generate_random_questions(db, instance_name, question_count)
     db.add(survey)
@@ -43,24 +46,22 @@ async def get_survey_results(survey_id: UUID):
     return {"id": survey_id}
 
 
-@app.get("/surveys/{survey_id}", response_model=QuestionResponse)
+@app.get("/surveys/{survey_id}", response_model=SurveyResponse)
 async def get_survey(survey_id: UUID, db: Session = Depends(get_db)):
-    question = Question()
-    question.content = "testquestion"
-    question.id = "testid19"
-    db.add(question)
-    db.commit()
-    db.flush()
-    return {
-        "content": question.content,
-    }
+    survey = utils.get_survey(db, str(survey_id))
+    return survey
 
 
 @app.post("/surveys/{survey_id}/answers", status_code=201)
-async def complete_the_survey(survey_id: UUID, survey_answers: SurveyAnswers):
-    score = 0.55
+async def complete_the_survey(survey_id: UUID, survey_answers: List[SurveyAnswersRequest],
+                              db: Session = Depends(get_db)):
+    survey = utils.get_survey(db, str(survey_id))
+    score = 0
+    survey_length = len(survey.questions)
+    for answer in survey_answers:
+        if is_answer_correct(db, answer.answer_id):
+            score += 1
     return {
-        "id": survey_id,
         "score": score,
-        "request body": survey_answers
+        "question_count": survey_length
     }
